8285b63e9a5af426e68fb517469f6484dc65dbf9
Egor Klimenok <e.klimenok@pixelplex.io>
2022-04-03 23:41:02 +0400

    home page base

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MainLayout } from "../MainLayout";
+
+import styles from "./styles.module.css";
+
+export const Home = () => {
+  return <MainLayout title="Marvel heroes"></MainLayout>;
+};
---
+++ src/types/mainLayout.type.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export type MainLayoutType = {
+  children: ReactNode;
+  title: string;
+};
