d90a6f289b3f9a9392ded408eb65a8d7d6dc585e
Egor Klimenok <e.klimenok@pixelplex.io>
2022-04-11 20:41:12 +0400

    rename Home directory

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -4,6 +4,7 @@ import { useMarvelHeroes } from "hooks/useMarvelHeroes";
 import { MarvelHeroRenderItemType } from "types/marvel-heroes.types";
 import MainLayout from "components/MainLayout";
 import Hero from "../Hero";
+import Link from "next/link";
 
 export const Home = () => {
   const { marvelHeroes } = useMarvelHeroes();
@@ -12,12 +13,15 @@ export const Home = () => {
     <MainLayout title="Marvel heroes">
       <div>
         {marvelHeroes.map((item: MarvelHeroRenderItemType) => (
-          <Hero
-            key={item.id}
-            name={item.name}
-            description={item.description}
-            src={`${item.thumbnail.path}/${IMAGE_RESOLUTIONS.STANDARD_LARGE}.${item.thumbnail.extension}`}
-          />
+          <Link passHref href={`/${item.id}`} key={item.id}>
+            <a>
+              <Hero
+                name={item.name}
+                description={item.description}
+                src={`${item.thumbnail.path}/${IMAGE_RESOLUTIONS.STANDARD_LARGE}.${item.thumbnail.extension}`}
+              />
+            </a>
+          </Link>
         ))}
       </div>
     </MainLayout>
